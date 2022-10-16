8b3c423a211844a8e89e7cde3d50d7792ff8db85
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-10-16 21:57:31 +0530

    basic function and input

---
+++ function basic/main.py
@@ -0,0 +1,16 @@
+class Main:
+
+  def my_function():
+    print("Hello from a function")
+
+  my_function()
+
+
+
+  # def  get_input():
+  #   user = input('Enter the user name :')
+  #   print('User Name:',user)
+
+  # get_input()
+  
+
